52977ce4266ae3821ec33b2facd33454869311d7
Zallyy <Zallybags@gmail.com>
2020-11-30 21:44:08 -0800

    Day 1 Done

--- 1-Report_Repair/2.js
+++ 1-Report_Repair/2.js
@@ -6,8 +6,10 @@ input = input.map(num => +num)
 
 for (let i = 0; i < input.length; i++) {
     for (let j = 0; j < input.length; j++) {
-        if (input[i] + input[j] == 2020) {
-            console.log(input[i] * input[j])
+        for (let k = 0; k < input.length; k++) {
+            if (input[i] + input[j] + input[k] == 2020) {
+                console.log(input[i] * input[j] * input[k])
+            }
         }
     }
 }
